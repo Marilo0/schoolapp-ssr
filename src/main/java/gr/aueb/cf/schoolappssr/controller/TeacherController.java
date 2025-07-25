7c421436f4a4e87fbe7019cf0ac6d7b95136ae16
Marilo0 <marialenabarbopoulou@gmail.com>
2025-07-25 20:50:23 +0300

    feat: add controllers,teacher-form &  move .css to static

---
+++ src/main/java/gr/aueb/cf/schoolappssr/controller/TeacherController.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.schoolappssr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/school/teachers")
+public class TeacherController {
+}
