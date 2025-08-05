14a8df4ebb7bfd507bc727817902bac701a30d21
Marilo0 <marialenabarbopoulou@gmail.com>
2025-08-05 15:26:04 +0300

    feat: add TeacherInsertDTO, regions.sql εÏÎ, trinsert.css, handle GET /insert in TeacherController

--- src/main/java/gr/aueb/cf/schoolappssr/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolappssr/controller/TeacherController.java
@@ -1,9 +1,27 @@
 package gr.aueb.cf.schoolappssr.controller;
 
+import gr.aueb.cf.schoolappssr.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolappssr.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/school/teachers")
 public class TeacherController {
+
+    private final RegionRepository regionRepository;
+    @Autowired
+    public TeacherController(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    @GetMapping("/insert")
+    public String getTeacherForm(Model model){
+        model.addAttribute("teacherInsertDTO", new TeacherInsertDTO());
+        model.addAttribute("regions", regionRepository.findAll());
+        return "teacher-form";
+    }
 }
