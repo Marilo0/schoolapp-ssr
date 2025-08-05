2d48f7909d616f1cc6ac12a5de8d03536bf1c88c
Marilo0 <marialenabarbopoulou@gmail.com>
2025-08-05 22:07:16 +0300

    feat: update mapper

--- src/main/java/gr/aueb/cf/schoolappssr/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolappssr/mapper/Mapper.java
@@ -1,6 +1,7 @@
 package gr.aueb.cf.schoolappssr.mapper;
 
 import gr.aueb.cf.schoolappssr.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolappssr.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolappssr.model.Teacher;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +10,10 @@ public class Mapper {
 
     public Teacher mapToTeacherEntity (TeacherInsertDTO dto) {
         return  new Teacher(null, null, dto.getVat(), dto.getFirstname(), dto.getLastname(), null);
+    }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO (Teacher teacher ) {
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(), teacher.getUpdatedAt(),
+                teacher.getFirstname(), teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
     }
 }
