767e12ad4e2d90fdd30be5a8bc575ce489e2a6e0
Marilo0 <marialenabarbopoulou@gmail.com>
2025-08-10 17:54:49 +0300

    feat: add UserRepository, CustomAuthProvider & CustomUserDetailsService

---
+++ src/main/java/gr/aueb/cf/schoolappssr/authentication/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.schoolappssr.authentication;
+
+import gr.aueb.cf.schoolappssr.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(()-> UsernameNotFoundException("User not found with username " + username));
+    }
+}
