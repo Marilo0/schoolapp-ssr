a3b3aaa12e77dfd1fbe6048a39896d8356f72756
Marilo0 <marialenabarbopoulou@gmail.com>
2025-08-12 15:14:14 +0300

    feat: add mapper

--- src/main/java/gr/aueb/cf/schoolappssr/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolappssr/mapper/Mapper.java
@@ -1,9 +1,12 @@
 package gr.aueb.cf.schoolappssr.mapper;
 
+import gr.aueb.cf.schoolappssr.core.enums.Role;
 import gr.aueb.cf.schoolappssr.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolappssr.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolappssr.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolappssr.dto.UserInsertDTO;
 import gr.aueb.cf.schoolappssr.model.Teacher;
+import gr.aueb.cf.schoolappssr.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,5 +24,15 @@ public class Mapper {
     public TeacherEditDTO mapToTeacherEditDTO( Teacher teacher){
         return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
                 teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
+    }
+
+    public User mapToUserEntity (UserInsertDTO userInsertDTO){
+//        return new User(userInsertDTO.getUsername(), userInsertDTO.getPassword(),
+//                Role.valueOf(userInsertDTO.getRole().toUpperCase())); //επειδή στον user το role είναι String , valueOf κανει typecast
+        return User.builder()
+                .username(userInsertDTO.getUsername())
+                .password(userInsertDTO.getPassword())
+                .role(Role.valueOf(userInsertDTO.getRole().toUpperCase()))
+                .build();
     }
 }
